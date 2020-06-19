94ca46c3806586990956e03efd5d215b66da012c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-18 23:24:32 -0300

    Estrutura e Estilo para Repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,45 @@ const Dashboard: React.FC = () => {
         <input type="text" placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://api.adorable.io/avatars/70/abott@adorable.png"
+            alt="Adorable Avatars"
+          />
+          <div>
+            <strong>Repositório</strong>
+            <p>Descrição do repositório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://api.adorable.io/avatars/70/abott@adorable.png"
+            alt="Adorable Avatars"
+          />
+          <div>
+            <strong>Repositório</strong>
+            <p>Descrição do repositório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://api.adorable.io/avatars/70/abott@adorable.png"
+            alt="Adorable Avatars"
+          />
+          <div>
+            <strong>Repositório</strong>
+            <p>Descrição do repositório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
